aa1bd301bf168a7c5539cae58982dc519ef59b10
shreya-nipanikar <nipanikar.shreya1@gmail.com>
2021-05-27 01:07:46 -0400

    my react website

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Contact = () => {
+  const [item, setItem] = useState({
+    name: '',
+    phonenumber: '',
+    email: '',
+    message: '',
+  });
 
-  const formSubmit = () => {
-    alert("Thankyou! Your message has been submitted. We will get back to you soon.");
+  const formAction = (event) => {
+    const {name,value} = event.target;
+
+    setItem((preVal)=>{
+      return {
+        ...preVal,
+        [name]: value,
+      }
+    })
+
   }
+
+  
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    alert(`Thankyou ${item.name} ! Your message has been submitted. We will get back to you soon.`);
+    setItem({
+      name: '',
+      phonenumber: '',
+      email: '',
+      message: '',
+    });
+  }
+
+  
     return (
         <>
         <div className="mycontainer">
@@ -18,20 +47,20 @@ const Contact = () => {
                 <form onSubmit={formSubmit}>
   <div className="form-group">
     <label className="mb-2">Name</label>
-    <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Enter Name" />
+    <input type="text" className="form-control" name="name" value={item.name} onChange={formAction} id="exampleFormControlInput1" placeholder="Enter Name" />
   </div>
   <div className="form-group my-3">
     <label  className="mb-2">Phone Number</label>
-    <input type="number" className="form-control" id="exampleFormControlInput1" placeholder="Enter Mobile Number" />
+    <input type="number" className="form-control" name="phonenumber" value={item.phonenumber} onChange={formAction} id="exampleFormControlInput1" placeholder="Enter Mobile Number" />
   </div>
   <div className="form-group my-3">
     <label className="mb-2">Email address</label>
-    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Enter Email" />
+    <input type="email" className="form-control"  name="email" value={item.email} onChange={formAction} id="exampleFormControlInput1" placeholder="Enter Email" />
   </div>
   
   <div className="form-group my-3">
     <label  className="mb-2">Message</label>
-    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    <textarea className="form-control" name="message" value={item.message} onChange={formAction} id="exampleFormControlTextarea1" rows="3"></textarea>
   </div>
 
   <div className="col-12 text-center">
